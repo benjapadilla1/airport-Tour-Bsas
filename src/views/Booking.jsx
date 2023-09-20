61434b20de4f23dd860fc4bd0d6a785556c87de5
Benjamin Padilla <benja.padilla@outlook.com.ar>
2023-09-20 18:28:01 -0300

    calendar

--- src/views/Booking.jsx
+++ src/views/Booking.jsx
@@ -1,5 +1,89 @@
+import { useState } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import { hoursOfDay } from "../utils/data";
+import { AiOutlineArrowLeft, AiOutlineWhatsApp } from "react-icons/ai";
 const Booking = () => {
-  return <div>Booking</div>;
+  const [selectedDate, setSelectedDate] = useState(null);
+  const [selectedTime, setSelectedTime] = useState(null);
+
+  const handleChange = (date) => {
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+
+    if (date > today) {
+      const isValid = selectedDate
+        ? date.toString() === selectedDate.toString()
+        : true;
+      if (isValid) {
+        setSelectedDate(date);
+      } else {
+        alert("Invalid date");
+      }
+    } else {
+      alert("You cannot choose a past or current date");
+    }
+  };
+  const handleTimeChange = (time) => {
+    setSelectedTime(time);
+  };
+  const handleSubmit = () => {
+    console.log(selectedDate, selectedTime);
+    if (selectedTime && selectedDate) {
+      const message = `Hello I would like to appoint a date on ${selectedDate.toLocaleDateString()} at ${selectedTime}`;
+
+      const WhatsappUrl = `https://api.whatsapp.com/send?phone=${
+        import.meta.env.VITE_PHONE_NUMBER
+      }&text=${encodeURIComponent(message)}`;
+
+      window.open(WhatsappUrl, "_blank");
+    } else {
+      alert("Please select a date and time before sending the message");
+    }
+  };
+  return (
+    <>
+      <h2 className="text-center text-3xl mt-16 mb-4">Book a Date</h2>
+      <div className="flex items-center justify-center flex-col gap-6">
+        {selectedDate ? (
+          <>
+            <button onClick={() => setSelectedDate(null)}>
+              <p className="text-center flex align-middle flex-row text-2xl">
+                <AiOutlineArrowLeft size={30} />
+                Choose another date
+              </p>
+            </button>
+            <p className="text-2xl">
+              Date: {selectedDate.toLocaleDateString()}
+            </p>
+            <p className="text-2xl">Select a Time:</p>
+            <select
+              onChange={(e) => handleTimeChange(e.target.value)}
+              value={selectedTime || ""}
+              className="text-xl"
+            >
+              <option value="" disabled hidden>
+                Time
+              </option>
+              {hoursOfDay.map((hour, i) => (
+                <option key={i} value={hour}>
+                  {hour}
+                </option>
+              ))}
+            </select>
+            <button onClick={handleSubmit}>
+              <p className="text-center flex align-middle flex-row text-2xl">
+                Book via Whatsapp
+                <AiOutlineWhatsApp size={30} color="green" />
+              </p>
+            </button>
+          </>
+        ) : (
+          <Calendar locale="en" onChange={handleChange} value={selectedDate} />
+        )}
+      </div>
+    </>
+  );
 };
 
 export default Booking;
--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -1,36 +1,5 @@
-import { useState } from "react";
-import Calendar from "react-calendar";
-import "react-calendar/dist/Calendar.css";
 const Contact = () => {
-  const [selectedDate, setSelectedDate] = useState([]);
-
-  const handleChange = (date) => {
-    const today = new Date();
-    today.setHours(0, 0, 0, 0);
-
-    if (date >= today) {
-      const isValid = selectedDate.some(
-        (selectedDate) => date.toDateString() === selectedDate.toDateString()
-      );
-      if (!isValid) {
-        setSelectedDate([...selectedDate, date]);
-        console.log(selectedDate);
-      } else {
-        alert("Invalid date");
-      }
-    } else {
-      alert("You cannot choose a past date");
-    }
-  };
-
-  return (
-    <>
-      <h2 className="text-center mt-16 mb-4">Book a Date</h2>
-      <div className="flex items-center justify-center ">
-        <Calendar onChange={handleChange} value={selectedDate} />
-      </div>
-    </>
-  );
+  return <div>Contact</div>;
 };
 
 export default Contact;

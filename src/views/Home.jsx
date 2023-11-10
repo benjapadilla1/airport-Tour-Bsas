daf303af28321300bf036ef95e2b6c3c10a961d3
RamiPiacen <ramipiacentini1@gmail.com>
2023-11-10 15:32:21 -0300

    imagen cambio

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -2,13 +2,8 @@ const Home = () => {
   return (
       <>
       <div className="ImagenContenedor">
-      <img className="imagenInicio" src="https://static.eldiario.es/clip/adec5662-9d7a-49c5-8a06-8c87a5155f86_16-9-discover-aspect-ratio_default_0.jpg"  alt="" />
-      <p className="parrafoPrincipal">hola</p>
+      <p className="parrafoPrincipal">Bienvenido Puto</p>
       </div>
-      <div>
-        
-      </div>
-      
       </>
   );
 };

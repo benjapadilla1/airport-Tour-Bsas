4af3fc9614290594e40253648c587b80d23bb54c
Benjamin Padilla <benja.padilla@outlook.com.ar>
2023-11-14 20:17:44 -0300

    bye bye

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,11 @@
-import { Route, Routes } from "react-router-dom";
-import Home from "./views/Home";
-import Tours from "./views/Tours";
-import Booking from "./views/Booking";
-import NavBar from "./components/NavBar/NavBar";
 import Footer from "./components/Footer/Footer";
+import NavBar from "./components/NavBar/NavBar";
 
 export default function Router() {
   return (
     <div className="navBar bg-gray  ">
       <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/tours" element={<Tours />} />
-        <Route path="/booking" element={<Booking />} />
-      </Routes>
+
       <Footer/>
     </div>
   );

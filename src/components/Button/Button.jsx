c1a5a6ebe51a98d4a464801987b93a85bab92da3
Benjamin Padilla <benja.padilla@outlook.com.ar>
2023-12-05 11:10:30 -0300

    wip

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -9,9 +9,9 @@ const Button = ({ onClick, children, tailwindClassName, backgroundColor, textCol
   return (
     <button onClick={onClick} className={`flex flex-row items-center ${tailwindClassName}`} style={buttonStyle} >
       <>
-      {iconLeft && <span className="mb-2">{iconLeft}</span>}
+      {iconLeft && <span className="mr-2">{iconLeft}</span>}
       {children}
-      {iconRight && <span className="mb-2">{iconRight}</span>}
+      {iconRight && <span className="ml-2">{iconRight}</span>}
       </>
     </button>
   )
--- src/components/Sections/Section4/index.jsx
+++ src/components/Sections/Section4/index.jsx
@@ -1,13 +1,13 @@
 import Button from "../../Button/Button"
-
+import bgFoto from "../../image/bgBuenosAires.jpg"
 const index = () => {
   return (
-    <div className="bg-cover bg-center flex" style={{ backgroundImage: 'url("https://static.eldiario.es/clip/adec5662-9d7a-49c5-8a06-8c87a5155f86_16-9-discover-aspect-ratio_default_0.jpg")' }} >
+    <div className="bg-contain bg-center flex" style={{ backgroundImage: `url(${bgFoto})` }} >
       <div className="w-1/2 p-16 flex justify-center align-middle flex-col">
-        <h1 className="text-6xl font-bold mb-4">Book a Taxi or Tour</h1>
-        <p className="text-lg mb-4">Explore Buenos Aires with our reliable taxi service and exciting tours</p>
+        <h1 className="text-white text-6xl font-bold mb-4">Book a Taxi or Tour</h1>
+        <p className="text-white text-lg mb-4 text-left">Explore Buenos Aires with our reliable taxi service and exciting tours</p>
         <div className="flex gap-4 justify-left">
-          <Button tailwindClassName="border border-solid border-black px-4 py-2" backgroundColor="black" textColor="black">WhatsApp</Button>
+          <Button tailwindClassName="border border-solid border-black px-4 py-2" backgroundColor="black" textColor="white">WhatsApp</Button>
           <Button tailwindClassName="px-4 py-2" backgroundColor="grey" textColor="black">
             Contact
           </Button>

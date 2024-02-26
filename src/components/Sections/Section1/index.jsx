7d518ebb6af989390d0c3aed7cd8705bab2fd029
benjapadilla1 <benja.padilla@outlook.com.ar>
2024-02-26 11:55:46 -0300

    .

--- src/components/Sections/Section1/index.jsx
+++ src/components/Sections/Section1/index.jsx
@@ -9,9 +9,11 @@ const Index = ({ id }) => {
         <p className="text-base lg:text-lg mb-4 lg:text-left">Transfers to and from Jorge Newbery Aeroparque.</p>
         <p className="text-base lg:text-lg mb-4 lg:text-left">Transfers to and from Cruise Terminals - Buquebus - Colonia Express - Long-distance Buses, etc.</p>
         <p className="text-base lg:text-lg mb-4 lg:text-left">With our professional and friendly taxi service, we offer convenient transportation options for all your travel needs.</p>
+        <div className="flex justify-center lg:justify-start">
         <button
           className="w-2/4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
           onClick={window.open("https://drive.google.com/drive/folders/19vyP1U_0mU68G7GZ2glkhdSjEqybiNMr", "_blank")}>See more photos</button>
+          </div>
       </div>
       <div className="lg:w-1/2 bg-cover bg-center h-64 lg:h-auto" style={{ backgroundImage: `url(${BgFoto})` }} />
     </div>
